4b812e515d55e09a615d6f763c27db866054e60c
luciameneghin <lucia.meneghin00@gmail.com>
2025-05-07 10:29:11 +0200

    feat: snack 3

--- test.js
+++ test.js
@@ -7,13 +7,22 @@ const getInitials = (name) => {
   return initials;
 }
 
-
 // Snack 2
 const createSlug = (stringa) => {
   const slug = stringa.toLowerCase();
   return slug;
 }
+
+//Snack 3
+const average = (array) => {
+  const sum = array.reduce((acc, num) => acc + num, 0);
+  const avg = sum / array.length;
+  return Number(avg.toFixed(1));
+}
+
+
 module.exports = {
   getInitials,
-  createSlug
+  createSlug,
+  average
 }
--- test.test.js
+++ test.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require("./test.js");
+const { getInitials, createSlug, average } = require("./test.js");
 
 // Snack 1 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
@@ -11,3 +11,9 @@ test("La funzione createSlug restituisce una stringa in lowercase.", () => {
   const result = createSlug('Hello World');
   expect(result).toBe('hello world')
 });
+
+// Snack 3
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+  const result = average([1, 4, 6, 9, 32, 12, 67])
+  expect(result).toBe(18.7)
+})
